4764b74521f8e15c3877344854d1a690159b9873
black <huang_jin_qiang@sina.com>
2013-09-17 22:11:26 +0800

    另一个工程也测试看看

---
+++ test.go
@@ -0,0 +1,15 @@
+// Title：
+//
+// Description:
+//
+// Author:black
+//
+// Createtime:2013-09-17 22:10
+//
+// Version:1.0
+//
+// 修改历史:版本号 修改日期 修改人 修改说明
+//
+// 1.0 2013-09-17 22:10 black 创建文档
+package lessgo
+
